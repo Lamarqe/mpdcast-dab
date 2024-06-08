f17ee62ae3d493c3c97c6c42973b8a2d948c4a61
Lamarqe <scritch@gmx.de>
2024-06-08 23:20:46 +0200

    Add files via upload

---
+++ src/welle-python/dabserver.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import asyncio
+from aiohttp import web
+
+import welle_lib
+
+
+class DabServer():
+  
+  def __init__(self, my_ip, port):
+    self.my_ip = my_ip
+    self.port = port
+    self.radio_controller = welle_lib.RadioController()
+    self.radio_controller.init()
+    self.handlers = {}
+
+  def get_routes(self):
+    return [web.get(r'/stream/{channel:[0-9]{1,2}[A-Z]}/{program:.+}', self.get_audio),
+            web.get(r'/image/current/{channel:[0-9]{1,2}[A-Z]}/{program:.+}', self.get_current_image),
+            web.get(r'/label/current/{channel:[0-9]{1,2}[A-Z]}/{program:.+}', self.get_current_label),
+            web.get(r'/image/next/{channel:[0-9]{1,2}[A-Z]}/{program:.+}', self.get_next_image),
+            web.get(r'/label/next/{channel:[0-9]{1,2}[A-Z]}/{program:.+}', self.get_next_label)]
+
+  async def stop(self):
+    await self.radio_controller.finalize()
+
+
+  # is_float should only be true if the audio data is in 32-bit floating-point format.
+  def wav_header(self, is_float, channels, bit_rate, sample_rate):
+    bo = 'little'; enc = 'ascii'
+          # RIFF header
+    return (bytes('RIFF', enc)                                         # Chunk ID
+          + (0).to_bytes(4, bo)                                        # Chunk size: stream -> set to zero
+          + bytes('WAVE', enc)                                         # Format
+
+          # Sub-chunk 1                                                
+          + bytes('fmt ', enc)                                         # Sub-chunk 1 ID
+          + (16).to_bytes(4, bo)                                       # Sub-chunk 1 size
+          + (3 if is_float else 1).to_bytes(2, bo)                     # Audio format (floating point (3) or PCM (1))
+          + channels.to_bytes(2, bo)                                   # Channels
+          + sample_rate.to_bytes(4, bo)                                # Sample rate
+          + (sample_rate * channels * (bit_rate // 8)).to_bytes(4, bo) # Bytes rate
+          + (channels * (bit_rate // 8)).to_bytes(2, bo)               # Block align
+          + bit_rate.to_bytes(2, bo)                                   # Bits per sample
+
+          # Sub-chunk 2                                            
+          + bytes('data', enc)                                         # Sub-chunk 2 ID
+          + (0).to_bytes(4, bo))                                       # Sub-chunk 2 size: stream -> set to zero
+
+  async def get_next_image(self, request):
+    channel = request.match_info['channel']
+    program = request.match_info['program'] 
+    print('get_next_image', channel, program)
+    if program in self.handlers:
+      try:
+        image = await self.handlers[program].new_picture()
+        return web.Response(body = image['data'],
+                            content_type = image['type'],
+                            headers={'Cache-Control': 'no-cache', 'Connection': 'Close'})
+      except welle_lib.UnsubscribedError:
+        raise web.HTTPBadRequest()
+    else:
+      raise web.HTTPNotFound()
+
+
+  async def get_next_label(self, request):
+    channel = request.match_info['channel']
+    program = request.match_info['program'] 
+    print('get_next_label', channel, program)
+    if program in self.handlers:
+      try:
+        label = await self.handlers[program].new_label()
+        return web.Response(text=label,
+                            headers={'Cache-Control': 'no-cache', 'Connection': 'Close'})
+      except welle_lib.UnsubscribedError:
+        raise web.HTTPBadRequest()
+    else:
+      raise web.HTTPNotFound()
+
+
+  async def get_current_image(self, request):
+    channel = request.match_info['channel']
+    program = request.match_info['program']  
+    print('get_current_image', channel, program)
+    if (program in self.handlers and
+        len(self.handlers[program].picture['data']) > 0):
+        return web.Response(body = self.handlers[program].picture['data'],
+                            content_type = self.handlers[program].picture['type'],
+                            headers={'Cache-Control': 'no-cache', 'Connection': 'Close'})
+    else:
+      raise web.HTTPNotFound()
+
+
+  async def get_current_label(self, request):
+    channel = request.match_info['channel']
+    program = request.match_info['program']  
+    print('get_current_label', channel, program)
+    if program in self.handlers:
+      return web.Response(text=self.handlers[program].label,
+                          headers={'Cache-Control': 'no-cache', 'Connection': 'Close'})
+    else:
+      raise web.HTTPNotFound()
+
+
+  async def get_audio(self, request, retry = True):
+    channel = request.match_info['channel']
+    program = request.match_info['program']  
+    if program.startswith('cover.'):
+      raise web.HTTPNotFound()
+    print('new audio request for', program)
+    
+
+    handler = await self.radio_controller.subscribe_program(channel, program)
+    if not handler:
+      # The device is busy with streaming another channel
+      if retry:
+        await asyncio.sleep(0.5)
+        await self.get_audio(request, False)
+      else:
+        raise web.HTTPServiceUnavailable()
+    
+    # from here on, the device sends us the audio stream
+    # send it via stream response until the user cancels it
+    self.handlers[program] = handler
+    try:
+      response = web.StreamResponse(
+        status=200,
+        reason='OK',
+        headers={'Content-Type': 'audio/wav','Cache-Control': 'no-cache', 'Connection': 'Close'})
+      await response.prepare(request)
+
+      # prepend the wav header to the initial response
+      next_audio_frame, audio = await handler.new_audio()
+      header = self.wav_header(False, 2, 16, handler.sample_rate)
+      await response.write(header + audio)
+
+      while True:
+        next_audio_frame, audio = await handler.new_audio(next_audio_frame)
+        await response.write(audio)
+#        await asyncio.sleep(0.1)  # consider using this for performance gains
+    except (asyncio.exceptions.CancelledError,
+            asyncio.exceptions.TimeoutError,
+            ConnectionResetError):
+      # user cancelled the stream, so unsubscribe
+      await self.radio_controller.unsubscribe_program(program)
+      if not self.radio_controller.is_playing(program):
+        del self.handlers[program]
+      return response
+    except welle_lib.UnsubscribedError:
+      return response
+
+    # Make sure above that this line remains unreachable 
+    raise web.HTTPInternalServerError()
---
+++ src/welle-python/welle_lib.py
@@ -0,0 +1,285 @@
+import asyncio
+import datetime
+import libwelle_py as c_lib
+
+class UnsubscribedError(Exception):
+  pass
+
+class WavProgrammeHandler():
+  BUFFER_SIZE = 10
+
+  def __init__(self, controller, sId):
+    self._controller = controller
+    self.sId = sId
+    self._subscribers = 0
+
+    self._next_frame = 0
+    self._audio_data = [b''] * WavProgrammeHandler.BUFFER_SIZE
+    self._audio_data_lock = asyncio.Lock()
+
+    # properties for most recent data. 
+    # Can be used directly by user applications to get the most recent data
+    self.picture = None
+    self.label   = ''
+    
+    # internal update notification events
+    self._audio_event      = asyncio.Event()
+    self._picture_event    = asyncio.Event()
+    self._label_event      = asyncio.Event()
+    
+    self._caller_loop = asyncio.get_running_loop()
+    self._delete_in_progress = False
+
+  # notification routines for user applications
+  async def new_audio(self, start_frame=0):
+    if start_frame == self._next_frame:
+      await self._audio_event.wait()
+      if self._delete_in_progress:
+        raise UnsubscribedError
+    async with self._audio_data_lock:
+      if start_frame < self._next_frame:
+        ret_list = self._audio_data[start_frame:self._next_frame]
+      else:
+        ret_list = self._audio_data[start_frame:] + self._audio_data[:self._next_frame]
+      audio_out = b''.join(ret_list)
+      return self._next_frame, audio_out
+
+  async def new_picture(self):
+    print('waiting for new picture')
+    await self._picture_event.wait()
+    if self._delete_in_progress:
+      raise UnsubscribedError
+    else:
+      print('forwarding new picture of type', self.picture['type'])
+      return self.picture
+
+  async def new_label(self):
+    print('waiting for new label')
+    await self._label_event.wait()
+    if self._delete_in_progress:
+      raise UnsubscribedError
+    else:
+      print('forwarding new label')
+      return self.label
+
+  def _release_waiters(self):
+    self._delete_in_progress = True
+    self._audio_event.set()
+    self._picture_event.set()
+    self._label_event.set()
+
+  def onFrameErrors(self, frameErrors):
+    pass
+
+  def onNewAudio(self, audio_data, sample_rate, mode):
+    self.sample_rate = sample_rate
+    asyncio.run_coroutine_threadsafe(self.buffer_audio(audio_data), self._caller_loop)
+
+  async def buffer_audio(self, audio_data):
+    async with self._audio_data_lock:
+      self._audio_data[self._next_frame] = audio_data
+      self._next_frame = (self._next_frame+1) % WavProgrammeHandler.BUFFER_SIZE
+    if not self._delete_in_progress:
+      self._audio_event.set()
+      self._audio_event.clear()
+
+  def onRsErrors(self, uncorrectedErrors, numCorrectedErrors):
+    pass
+
+  def onAacErrors(self, aacErrors):
+    pass
+
+  def onNewDynamicLabel(self, label):
+    self.label = label
+    asyncio.run_coroutine_threadsafe(self._set_event(self._label_event), self._caller_loop)
+    
+  async def _set_event(self, event):
+    if not self._delete_in_progress:
+      event.set()
+      event.clear()
+
+  def onMOT(self, data, mime_type, name):
+    self.picture = {'type': mime_type, 'data': data, 'name': name}
+    asyncio.run_coroutine_threadsafe(self._set_event(self._picture_event), self._caller_loop)
+
+  def onPADLengthError(self, announced_xpad_len, xpad_len):
+    pass
+
+
+class RadioController():
+  PROGRAM_DISCOVERY_TIMEOUT = 10
+  
+  def __init__(self, gain=-1):
+#    test = c_lib.RadioController()
+#    test.WPH_Test()
+    self.gain = gain    
+    self.programs            = {}
+    self._programme_handlers = {}
+
+    self._current_channel = ""
+    self.ensemble_label   = None
+
+    # lock to prevent parallel initialization from multiple users
+    self._subscription_lock = asyncio.Lock()
+
+    
+  # Note: This method must not be called by __init__, as self cannot yet be used at this point
+  def init(self, device_name = "auto"):
+    self.c_impl = c_lib.init_device(self, device_name, self.gain)
+
+  def onSNR(self, snr):
+    pass
+    
+  def onFrequencyCorrectorChange(self, fine, coarse):
+    pass
+    
+  def onSyncChange(self, isSync):
+    if isSync[0]:
+      pass
+    
+  def onSignalPresence(self, isSignal):
+    pass
+    
+  def onServiceDetected(self, sId):
+    if not sId in self.programs:
+      self.programs[sId] = None
+    
+  def onNewEnsemble(self, eId):
+    pass
+    
+  def onSetEnsembleLabel(self, label):
+    self.ensemble_label = label
+
+  def onDateTimeUpdate(self, timestamp):
+    self.datetime = datetime.datetime.fromtimestamp(timestamp)
+
+  def onFIBDecodeSuccess(self, crcCheckOk, fib):
+    pass
+    
+  def onMessage(self, text, text2, isError):
+    pass
+    
+  def _get_program_id(self, program_name):
+    for sId in self.programs:
+      if not self.programs[sId] or len(self.programs[sId]) == 0:
+        self.programs[sId] = c_lib.get_service_name(self.c_impl, sId).rstrip()
+      if self.programs[sId] == program_name:
+        return sId
+    # Not found
+    return None
+
+
+  async def _wait_for_channel(self, program_name):
+    # initial check, as we might already have an active subscription for the program
+    program_pid = self._get_program_id(program_name)
+    if program_pid:
+      return program_pid
+    
+    # wait the defined time for the program discovery 
+    # and check every 0.5 seconds if it was succesful
+    for _ in range(2 * RadioController.PROGRAM_DISCOVERY_TIMEOUT):
+      await asyncio.sleep(0.5)
+      program_pid = self._get_program_id(program_name)
+      if program_pid:
+        return program_pid
+    # Not found
+    return None
+      
+
+  # returns handler in case the subscription suceeded, otherwise None
+  async def subscribe_program(self, channel, program_name):
+    async with self._subscription_lock:
+      # Block actions in case there is another channel active
+      if self._current_channel and self._current_channel != channel:
+        return None
+
+      # If There is no active channel, tune the device to the channel
+      if not self._current_channel:
+        tune_okay = c_lib.set_channel(self.c_impl, channel)
+        if tune_okay:
+          self._current_channel = channel
+        else:
+          print("could not start device, fatal")
+          return None
+
+      # Wait for the selected program to appear in the channel
+      try:
+        program_pid = await self._wait_for_channel(program_name)
+
+      # Because the user might cancel the subscription request while waiting,
+      # we need to check for CancelledError and ConnectionResetError.
+      # In these cases, we need to reset the c lib to get back to an idle state. 
+      except (asyncio.exceptions.CancelledError,
+            ConnectionResetError):
+        if not self._programme_handlers:
+          await self._reset()
+				# re-throw the exception so the caller can also do its cleanup
+        raise
+
+      # The program is not part of the channel
+      if not program_pid:
+        if not self._programme_handlers:
+          await self._reset()
+        return None
+       
+      # the program exists in the channel. Check if there is already an active subscription
+      if program_pid in self._programme_handlers:
+        programme_handler = self._programme_handlers[program_pid]
+      else:
+        # First time subscription to the channel. Set up the handler and register it.
+        programme_handler = WavProgrammeHandler(self, program_pid)
+        self._programme_handlers[program_pid] = programme_handler
+        if not c_lib.subscribe_program(self.c_impl, programme_handler, program_pid):
+          if not self._programme_handlers:
+            await self._reset()
+          return None
+
+      # increase the counter of active subscriptions for the selected program
+      programme_handler._subscribers += 1
+      print('subscribers:', programme_handler._subscribers)
+      return programme_handler
+
+
+  async def unsubscribe_program(self, program_name):
+    async with self._subscription_lock:
+      program_pid = self._get_program_id(program_name)
+      if program_pid:
+        await self._unsubscribe(program_pid)
+
+
+  def is_playing(self, program_name):
+    program_pid = self._get_program_id(program_name)
+    if not program_pid:
+      return False
+    else:
+      return program_pid in self._programme_handlers
+
+
+  async def _unsubscribe(self, program_pid):
+    programme_handler = self._programme_handlers[program_pid]
+    if not programme_handler:
+      return
+
+    programme_handler._subscribers -= 1
+    print('subscribers:', programme_handler._subscribers)
+    if programme_handler._subscribers == 0:
+      c_lib.unsubscribe_program(self.c_impl, program_pid)
+      self._programme_handlers[program_pid]._release_waiters()
+      del self._programme_handlers[program_pid]
+      await asyncio.sleep(1)
+      if not self._programme_handlers:
+        await self._reset()
+
+  async def _reset(self):
+    c_lib.set_channel(self.c_impl, "")
+    self._current_channel = None
+    self.programs.clear()
+    await asyncio.sleep(1)
+  
+  async def finalize(self):
+    active_pids = list(self._programme_handlers.keys())
+    for program_pid in active_pids:
+      await self._unsubscribe(program_pid)
+    c_lib.close_device(self.c_impl)
+    c_lib.finalize(self.c_impl)
+    
